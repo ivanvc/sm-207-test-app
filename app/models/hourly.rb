2544f7ef417c2302e0e9ed2c08c2109f4963d5df
Ivan Valdes (@ivanvc) <iv@nvald.es>
2013-05-13 20:09:36 -0500

    Remove uneeded extra code

--- app/models/hourly.rb
+++ app/models/hourly.rb
@@ -1,6 +1,5 @@
 class Hourly < ActiveRecord::Base
   attr_accessible :timestamp
-  validates :timestamp, uniqueness: { scope: :source_id }, presence: true
-  validates :source_id, presence: true
+  validates :timestamp, uniqueness: { scope: :source_id }
   belongs_to :source
 end
